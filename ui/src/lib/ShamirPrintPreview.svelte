80d007e3021a88cc5c5239a431a75e77b4aacfb3
Braden Rayhorn <25675893+bradenrayhorn@users.noreply.github.com>
2025-12-13 17:27:39 -0600

    feat: add 3x5 index card print format (#35)
    closes #10

--- ui/src/lib/ShamirPrintPreview.svelte
+++ ui/src/lib/ShamirPrintPreview.svelte
@@ -13,6 +13,7 @@
 
   let qrSize = $state(100);
   let isShowingKey = $state(false);
+  let printFormat = $state("standard");
 </script>
 
 <div>
@@ -41,36 +42,63 @@
 
       <button onclick={() => (isShowingKey = true)}>SHOW ENCRYPTION KEY</button>
 
+      <select bind:value={printFormat}>
+        <option value="standard">Standard paper</option>
+        <option value="indexcard">Index cards</option>
+      </select>
+
       <p class="wrap">
         <b>Important!</b>
         Print this page, then write the encryption key on each printed share.
       </p>
     </div>
 
-    <div
-      class="code-list"
-      style="grid-template-rows: repeat({textShares.length}, 1fr) 0 2rem repeat({textShares.length}, 1fr)"
-    >
-      {#each textShares as share, i (i)}
-        <div aria-label={`Text share ${i + 1}`} class="share">
-          {share}
-        </div>
-      {/each}
-
-      <div class="pagebreak"></div>
-
-      <!-- Spacer given height in grid-template-rows style above. -->
-      <div></div>
-
-      {#each qrShares as svg, i (i)}
-        <div class="code">
-          <div style:width={`${qrSize}px`}>
-            <!-- eslint-disable svelte/no-at-html-tags -->
-            {@html svg}
+    {#if printFormat === "standard"}
+      <div class="code-list standard" style="--shares: {textShares.length}">
+        {#each textShares as share, i (i)}
+          <div aria-label={`Text share ${i + 1}`} class="share">
+            {share}
           </div>
-        </div>
-      {/each}
-    </div>
+        {/each}
+
+        <div class="pagebreak"></div>
+
+        <!-- Spacer given height in grid-template-rows style above. -->
+        <div></div>
+
+        {#each qrShares as svg, i (i)}
+          <div class="code">
+            <div style:width={`${qrSize}px`}>
+              <!-- eslint-disable svelte/no-at-html-tags -->
+              {@html svg}
+            </div>
+          </div>
+        {/each}
+      </div>
+    {:else if printFormat === "indexcard"}
+      <div class="code-list indexcard" style="--shares: {textShares.length}">
+        {#each textShares as share, i (i)}
+          <div class="face">
+            <div aria-label={`Text share ${i + 1}`} class="share">
+              {share}
+            </div>
+          </div>
+
+          <div class="pagebreak"></div>
+
+          <div class="face">
+            <div style:width={`${qrSize}px`}>
+              <!-- eslint-disable svelte/no-at-html-tags -->
+              {@html qrShares[i]}
+            </div>
+          </div>
+
+          {#if i < textShares.length - 1}
+            <div class="pagebreak"></div>
+          {/if}
+        {/each}
+      </div>
+    {/if}
   {/if}
 </div>
 
@@ -94,18 +122,37 @@
     display: grid;
     grid-auto-rows: 1fr;
     padding: 0 3rem 0 3rem;
-    padding-top: 2rem;
+
+    &.standard {
+      padding-top: 2rem;
+      grid-template-rows: repeat(var(--shares), 1fr) 0 2rem repeat(
+          var(--shares),
+          1fr
+        );
+
+      & .code {
+        text-align: right;
+        justify-content: flex-end;
+        padding: 0.5rem 0;
+      }
+    }
+    &.indexcard {
+      grid-template-rows: repeat(var(--shares), 1fr 0 1fr 0);
+
+      & .face {
+        justify-content: center;
+        height: 100vh;
+
+        & > div {
+          transform: rotate(270deg);
+        }
+      }
+    }
   }
 
   .code-list > div {
     display: flex;
     align-items: center;
-  }
-
-  .code {
-    text-align: right;
-    justify-content: flex-end;
-    padding: 0.5rem 0;
   }
 
   .menu {
